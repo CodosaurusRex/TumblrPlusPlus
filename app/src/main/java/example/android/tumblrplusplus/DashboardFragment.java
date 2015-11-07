32194f6e5fbe59277a202b2a7d563391dbf7df8a
Alex Chen <haskellalex2015@gmail.com>
2015-11-06 21:36:51 -0500

    receiving data from API!

--- app/src/main/java/example/android/tumblrplusplus/DashboardFragment.java
+++ app/src/main/java/example/android/tumblrplusplus/DashboardFragment.java
@@ -1,10 +1,13 @@
 package example.android.tumblrplusplus;
 
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -17,6 +20,15 @@ public class DashboardFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_dashboard, container, false);
+        View dashView = inflater.inflate(R.layout.fragment_dashboard, container, false);
+        Log.e("USER", "we are in OnCreateViewDashboardFragment");
+        Intent intent = getActivity().getIntent();
+        if (intent != null) {
+            String name = intent.getStringExtra("user");
+            Log.e("USER", "got name" + name );
+            ((TextView) dashView.findViewById(R.id.name)).setText(name);
+        }
+
+        return dashView;
     }
 }
--- app/src/main/java/example/android/tumblrplusplus/MainActivity.java
+++ app/src/main/java/example/android/tumblrplusplus/MainActivity.java
@@ -1,32 +1,52 @@
 package example.android.tumblrplusplus;
 
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
 import com.tumblr.jumblr.JumblrClient;
+import com.tumblr.jumblr.exceptions.JumblrException;
+import com.tumblr.jumblr.types.Post;
+import com.tumblr.jumblr.types.TextPost;
 import com.tumblr.jumblr.types.User;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private final String consumerKey = "Omq1FerYKMWeZnlvrIH9Qy3r6YIbyVDPdkQSfU5obu8eJBnt5n";
 
     private final String consumerSecret = "GHqE8rxq6r0IXCbBkj9NPR4ed0EIBqb8xP9k6PdulMuwsxJfyo";
 
-    private final String oAuthToken = "5AX3lj6EjPUVTbMOKvuMHBPb8M4NWZN5kerNXo4v7RYmzPKXCC";
 
-    private final String oAuthSecret = "KsvGQwzzuKM1fv7jMNdEhDqH1NwpJL7JB6AUoxxBEfweLKh6np";
+    private final String oAuthToken = "WMFunAyCRwm9Nuelcgb0P8dWR449tNvzGQconaM5GfbrT5De9r";
 
+    private final String oAuthSecret = "DCtLxt1ajr5yHfDcLkt7nMWzl9E8CquxEc85UbTbJJAq8Dkt6r";
+
+
+    User user;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         Log.e("USER", "creating");
 // Create a new client
         FetchTumblrsStuff fetch = new FetchTumblrsStuff();
         fetch.execute();
+        // Set the text view as the activity layout
         setContentView(R.layout.activity_main);
+        TextView text = (TextView) findViewById(R.id.myText);
+        text.setText(Html.fromHtml("Iowa City IA (SPX) Nov 06, 2015<br/><img src=\"http://www.spxdaily.com/images-bg/march-2015-sun-cme-compress-mars-magnetosphere-bg.jpg\" hspace=\"5\" vspace=\"2\" align=\"right\" border=\"0\" width=\"160\" height=\"128\"/>\n" +
+                "    Mars has been all over the news, from the blockbuster finding of seasonal water on the Red Planet to the wildly successful film, The Martian. Now, researchers&ndash;including those at the University of Iowa&ndash;have learned more about what happened to the climate on Mars since it was a warm and watery planet billions of years ago.\n" +
+                "    The researchers announced on Thursday that NASA&rsquo;s MAVEN (Mars Atmos<br/><a href=\"http://www.marsdaily.com/reports/Martian_desiccation_999.html\">Full article</a>"));
+        text.setMovementMethod(LinkMovementMethod.getInstance());
     }
 
     @Override
@@ -52,23 +72,105 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    public class FetchTumblrsStuff extends AsyncTask<Void, Void, Void> {
+    private void openDash(List<Post> posts){
+        try {
+            Intent intent = new Intent(this, Dashboard.class);
+            for (int i = 0; i < posts.size(); i++) {
+                Post post = (Post) posts.get(i);
+                String postStr = post.toString();
+                //Log.e("Post Type", post.getType());
+                //Log.e("Author", post.getAuthorId());
+                if (post.getType().equals("text")) {
+                    String tit = ((TextPost) post).getTitle();
+                    Log.e("TEXTPOST", postStr);
+                    if (tit != null)
+                        Log.e("Text Post title ", tit);
+                    Log.e("Text Post Body ", ((TextPost) post).getBody());
+                    View thisLayout = findViewById(R.id.thisLayout);
+                    TextView text = new TextView(this);
+                    text.setText(((TextPost) post).getBody());
+                    text.setMovementMethod(LinkMovementMethod.getInstance());
+                }
+
+            }
+        }catch(Exception ex){
+            Log.e("Error", ex.toString());
+        }
+//        startActivity(intent);
+    }
+
+    //start classes and such
+
+
+    public class FetchTumblrsStuff extends AsyncTask<Void, Void, List<Post>> {
 
         @Override
-        protected Void doInBackground(Void...params) {
-
+        protected List<Post> doInBackground(Void...params) {
             Log.e("USER", "fetching");
             JumblrClient client = new JumblrClient(consumerKey, consumerSecret);
-            //client.setToken(oAuthToken, oAuthSecret);
-            client.setToken(
-                    "joOletjix2MLiK9vMGybboTH8bQh6WjAfE19YI1yzydhcBl9q7",
-                    "g5K2VnsRD08hxPGfK4FjdfBQdOMgmPGh7YLNt6KC91cCIqaXPE"
-            );
-            User user = client.user();
-            Log.e("USER", user.getName());
-            return (null);
+            client.setToken(oAuthToken, oAuthSecret);
+            Log.e("USER", "got token");
+            List<Post> posts = client.userDashboard();
+            Log.e("USER Dash", "do in back");
+            return (posts);
         }
 
 
+
+
+        @Override
+        protected void onPostExecute(List<Post> posts) {
+            Log.e("USER", "post");
+            openDash(posts);
+
+        }
+
     }
+
+/*    public class Parser{
+        String target;
+        public Parser(String parseMe){
+
+            target = parseMe;
+
+        }
+
+        public ArrayList<myPost> parseText(String s){
+
+
+        }
+
+
+    }*/
+
+/*    public class myPost{
+        public myPost(String data){
+
+        }
+
+    }*/
+/*
+    public class myTextPost extends myPost{
+
+        String body;
+        String title;
+        public myTextPost(String givenbody, String gtitle){
+            super(body);
+            this.body = givenbody;
+            this.title = gtitle;
+        }
+
+        private ArrayList<String> split{
+            //was going to inherit string, but can't? oh well. clearly a java noob. rip.
+            ArrayList<String> ls= new ArrayList<String>();
+            ls.add("<t>" + title);
+            ls.add("");
+
+        }
+
+
+    }*/
+
+
 }
+
